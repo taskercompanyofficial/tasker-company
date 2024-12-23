3a957e390c1cbbc3eae87941bdca487e2a6e4f0b
taskercompanyofficial <taskercompany7@gmail.com>
2024-12-23 18:23:06 +0500

    Enhance Dashboard component with loading state and error handling
    - Integrated Suspense and Skeleton components to improve user experience during data fetching.
    - Added error handling to display a user-friendly message when data retrieval fails.
    - Updated imports for better organization and clarity in the Dashboard component.

--- src/app/(dashboard)/dashboard/tabs/dashboard.tsx
+++ src/app/(dashboard)/dashboard/tabs/dashboard.tsx
@@ -1,9 +1,11 @@
 import FirstChart from "@/components/dashboard/charts/FirstChart";
 import SecondChart from "@/components/dashboard/charts/second-chart";
-import React from "react";
+import React, { Suspense } from "react";
 import { fetchData } from "@/app/dataFetch/fetchData";
 import { API_URL, COMPLAINTS } from "@/lib/apiEndPoints";
 import Table from "../../complaints/components/Table";
+import { Skeleton } from "@/components/ui/skeleton";
+import { LuServerCrash } from "react-icons/lu";
 export default async function Dashboard({
   searchParams,
 }: {
@@ -21,7 +23,18 @@ export default async function Dashboard({
     <div className="space-y-4">
       <FirstChart data={complaintsByStatus?.data} />
       <SecondChart data={complaintsByStatus?.data} />
-      <Table data={response.data} endPoint={EndPoint} />
+      <Suspense fallback={<Skeleton className="h-48 w-full" />}>
+        {response.data ? (
+          <Table data={response.data} endPoint={EndPoint} />
+        ) : (
+          <div className="flex min-h-72 w-full flex-col items-center justify-center rounded border border-gray-200 bg-white px-2 py-4 text-gray-100 shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-600">
+            <div className="text-[70px]">
+              <LuServerCrash />
+            </div>
+            <div>Something went wrong please try to refresh the page.</div>
+          </div>
+        )}
+      </Suspense>
     </div>
   );
 }
