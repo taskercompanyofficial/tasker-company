88555b71847357d68401b53a9ed5f98fd24e3b20
TaskerCompany <taskercompany7@gmail.com>
2024-12-26 21:16:27 +0500

    Update SecondChart component layout to enhance visual representation of active complaints
    - Adjusted grid layout from 2 to 3 columns for better responsiveness.
    - Wrapped ActiveComplaintsChart in a div to control column span, improving overall structure.

--- src/components/dashboard/charts/second-chart.tsx
+++ src/components/dashboard/charts/second-chart.tsx
@@ -4,9 +4,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function SecondChart({ data }: { data: any }) {
   return (
-    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
       <Suspense fallback={<Skeleton className="h-[200px] w-full" />}>
-        <ActiveComplaintsChart chartData={data?.active_complaints} />
+        <div className="col-span-2">
+
+          <ActiveComplaintsChart chartData={data?.active_complaints} />
+        </div>
       </Suspense>
     </div>
   );
