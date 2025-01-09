8133995b4bea6512f4bce643815b1f47873ec5a7
Tasker Company <taskercompany7@gmail.com>
2025-01-09 16:53:15 +0500

    Update user navigation to display username instead of name for improved clarity

--- src/components/admin-panel/user-nav.tsx
+++ src/components/admin-panel/user-nav.tsx
@@ -58,7 +58,7 @@ export function UserNav({ user }: { user: typeOfUser | null }) {
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">
-              {user?.name ? user.name : "John Doe"}
+              {user?.username ? user.username : "John Doe"}
             </p>
             <p className="text-xs leading-none text-muted-foreground">
               {user?.email ? user.email : "john-doe@domain.com"}
